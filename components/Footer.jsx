9d379e994a5c5ddbd7309d678bcd92fcceb27174
matt <r014882k@student.staffs.ac.uk>
2024-05-04 02:00:55 +0100

    Installed Next.JS and DaisyUI

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <aside>
+        <p>WEBAI - Matthew Roche - R014882K</p>
+      </aside>
+    </footer>
+  );
+};
---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="navbar bg-base-100">
+    <a className="btn btn-ghost text-xl">AI Image Detection</a>
+  </div>
+  );
+};
